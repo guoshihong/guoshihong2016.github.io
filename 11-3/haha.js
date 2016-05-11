077f2eb925a1407f1ce86a34bbf948de19d100ca
guoshihong2016 <1459665107@qq.com>
2016-05-11 10:46:38 +0800

    11-3

---
+++ 11-3/haha.js
@@ -0,0 +1,17 @@
+//alert("haha");
+var age=19;
+alert(age);
+console.log(age);
+{
+	var s1="我"+"爱"+"你"；
+	console.log(s1);
+}
+if (age>12){console.log(老腊肉);
+
+}else{
+	console.log(小屁孩);
+
+}
+for(var i=0;i<5;i++)	{
+	console.log(i);
+}
